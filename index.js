d4e8c8214518ae4db774f8b887114b52f7eab495
LJ-Fryer <ljf_aus@hotmail.com>
2025-05-26 17:49:02 +1000

    Completed

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log("Testing");
-
 const bands = [
   "The Plot in You",
   "The Devil Wears Prada",
@@ -15,3 +13,12 @@ const bands = [
   "Anywhere But Here",
   "An Old Dog",
 ];
+function strip(bandName) {
+  return bandName.replace(/^(a |an |the)/i, " ").trim();
+}
+
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+
+document.querySelector("#bands").innerHTML = sortedBands
+  .map((band) => `<li>${band}</li>`)
+  .join(""); // removes , from the returned string
